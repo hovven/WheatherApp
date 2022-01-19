994897bfb8d64b056c55bd0a138563a527906f50
Hossein Vesali Naseh <hvesalinaseh@ebay.com>
2022-01-19 10:50:44 +0100

    initial commit

---
+++ Shared/WheaterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheaterAppApp.swift
+//  Shared
+//
+//  Created by hvesalinaseh on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WheaterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
